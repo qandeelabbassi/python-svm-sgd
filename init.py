bc2a55c9131ccae36cbef8f6c74eab05b700530e
Qandeel Abbassi <qandeelabbassiofficial@gmail.com>
2020-02-07 23:56:32 +0500

    added intercept term

--- init.py
+++ init.py
@@ -3,10 +3,11 @@ import pandas as pd
 import statsmodels.api as sm
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split as tts
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, recall_score, precision_score
+from sklearn.utils import shuffle
 
 
-# >> FEATURE ENGINEERING << #
+# >> FEATURE SELECTION << #
 def remove_correlated_features(X):
     corr_threshold = 0.9
     corr = X.corr()
@@ -42,49 +43,62 @@ def remove_less_significant_features(X, Y):
 
 # >> MODEL TRAINING << #
 def compute_cost(W, X, Y):
-    cost = 1 / 2 * np.dot(W, W)
+    # calculate hinge loss
+    N = X.shape[0]
     distances = 1 - Y * (np.dot(X, W))
-    distances[distances < 0] = 0
-    hinge_loss = np.sum(regularization_strength * distances)
-    hinge_loss = hinge_loss / X.shape[0]
-    cost += hinge_loss
+    distances[distances < 0] = 0  # equivalent to max(0, distance)
+    hinge_loss = regularization_strength * (np.sum(distances) / N)
+
+    # calculate cost
+    cost = 1 / 2 * np.dot(W, W) + hinge_loss
     return cost
 
 
-def calculate_cost_derivative(W, X_batch, Y_batch):
-    # if only one example then convert it to array
+# I haven't tested it but this same function should work for
+# vanilla and mini-batch gradient descent as well
+def calculate_cost_gradient(W, X_batch, Y_batch):
+    # if only one example is passed (eg. in case of SGD)
     if type(Y_batch) == np.float64:
         Y_batch = np.array([Y_batch])
-        X_batch = np.array([X_batch])
+        X_batch = np.array([X_batch])  # gives multidimensional array
 
     distance = 1 - (Y_batch * np.dot(X_batch, W))
-    derivative = 0
+    dw = np.zeros(len(W))
 
     for ind, d in enumerate(distance):
-        if d > 0:
-            di = W - (regularization_strength * Y_batch[ind] * X_batch[ind])
-        else:
+        if max(0, d) == 0:
             di = W
-        derivative += di
+        else:
+            di = W - (regularization_strength * Y_batch[ind] * X_batch[ind])
+        dw += di
 
-    derivative = derivative/len(Y_batch)  # average
-    return derivative
+    dw = dw/len(Y_batch)  # average
+    return dw
 
 
-def fit_svm(features, outputs):
-    epochs = 500000
-    rows = features.shape[0]
+def sgd(features, outputs):
+    max_epochs = 5000
     weights = np.zeros(features.shape[1])
     nth = 0
+    prev_cost = float("inf")
+    cost_threshold = 0.01  # in percent
     # stochastic gradient descent
-    for epoch in range(1, epochs):
-        rand_row = np.random.randint(rows)
-        ascent = calculate_cost_derivative(weights, features[rand_row], outputs[rand_row])
-        weights = weights - (learning_rate * ascent)
-        if epoch == 2 ** nth:
+    for epoch in range(1, max_epochs):
+        # shuffle to prevent repeating update cycles
+        X, Y = shuffle(features, outputs)
+        for ind, x in enumerate(X):
+            ascent = calculate_cost_gradient(weights, x, Y[ind])
+            weights = weights - (learning_rate * ascent)
+
+        # convergence check on 2^nth epoch
+        if epoch == 2 ** nth or epoch == max_epochs - 1:
             cost = compute_cost(weights, features, outputs)
-            nth += 1
             print("Epoch is: {} and Cost is: {}".format(epoch, cost))
+            # stoppage criterion
+            if abs(prev_cost - cost) < cost_threshold * prev_cost:
+                return weights
+            prev_cost = cost
+            nth += 1
     return weights
 
 
@@ -96,7 +110,8 @@ def init():
     # read data in pandas (pd) data frame
     data = pd.read_csv('./data/data.csv')
 
-    # drop last column (extra column added by pd) and first column (id)
+    # drop last column (extra column added by pd)
+    # and unnecessary first column (id)
     data.drop(data.columns[[-1, 0]], axis=1, inplace=True)
 
     print("applying feature engineering...")
@@ -116,30 +131,34 @@ def init():
     X_normalized = MinMaxScaler().fit_transform(X.values)
     X = pd.DataFrame(X_normalized)
 
+    # insert 1 in every row for intercept b
+    X.insert(loc=len(X.columns), column='intercept', value=1)
+
     # split data into train and test set
     print("splitting dataset into train and test sets...")
     X_train, X_test, y_train, y_test = tts(X, Y, test_size=0.2, random_state=42)
 
     # train the model
     print("training started...")
-    W = fit_svm(X_train.to_numpy(), y_train.to_numpy())
+    W = sgd(X_train.to_numpy(), y_train.to_numpy())
     print("training finished.")
     print("weights are: {}".format(W))
 
-    # check accuracy
-    print("checking accuracy...")
-    train_prediction = np.array([])
-    test_prediction = np.array([])
+    # testing the model
+    print("testing the model...")
+    y_train_predicted = np.array([])
     for i in range(X_train.shape[0]):
-        yp = np.sign(np.dot(W, X_train.iloc[i].values))
-        train_prediction = np.append(train_prediction, yp)
+        yp = np.sign(np.dot(X_train.to_numpy()[i], W))
+        y_train_predicted = np.append(y_train_predicted, yp)
 
+    y_test_predicted = np.array([])
     for i in range(X_test.shape[0]):
-        yp = np.sign(np.dot(W, X_test.iloc[i].values))
-        test_prediction = np.append(test_prediction, yp)
+        yp = np.sign(np.dot(X_test.to_numpy()[i], W))
+        y_test_predicted = np.append(y_test_predicted, yp)
 
-    print("accuracy on train dataset: {}".format(accuracy_score(y_train.to_numpy(), train_prediction)))
-    print("accuracy on test dataset: {}".format(accuracy_score(y_test.to_numpy(), test_prediction)))
+    print("accuracy on test dataset: {}".format(accuracy_score(y_test, y_test_predicted)))
+    print("recall on test dataset: {}".format(recall_score(y_test, y_test_predicted)))
+    print("precision on test dataset: {}".format(recall_score(y_test, y_test_predicted)))
 
 
 # set hyper-parameters and call init
